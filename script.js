be72f3bcc7097abc826197be0a1de5d48b1b8023
MohammedDwekat <mohammed.dwekat94@gmail.com>
2024-10-23 00:15:50 +0300

    test the script

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 import { Books } from "./data.js";
 
 const ViewBooks = (array = []) => {
-  console.log("Books list :\n");
-  array.forEach((Book) =>
-    console.log(` Book ${Book.id} ${Book.title} ,  ${Book.author}`)
-  );
+  //console.log("Books list :\n");
+
+  const booklist = document.getElementById("book-list");
+  array.forEach((Book) => {
+    //console.log(` Book ${Book.id} ${Book.title} ,  ${Book.author}`)
+
+    const NewBook = `<div><h4>${Book.title}\n</h4> <h5>${Book.author}\n</h5> <p> ${Book.description}</p>  </div>`;
+
+    booklist.innerHTML += NewBook;
+  });
 };
 
 ViewBooks(Books);
