d1c63073ec001d28946ca8f25523e4fa96883a85
MohammedDwekat <mohammed.dwekat94@gmail.com>
2024-10-23 22:11:28 +0300

    update data frile - add price to the books

--- data.js
+++ data.js
@@ -1,204 +1,4 @@
-export const Books = [
-  {
-    id: 1,
-    title: "The Enchanted Forest",
-    author: "Alice Green",
-    genre: "Fantasy",
-    description:
-      "A captivating tale of a young girl's journey through a magical forest full of strange creatures and hidden secrets.",
-    status: "read",
-    image: "/mnt/data/A_single_fantasy_book_cover_titled_'The_Enchanted_.png",
-  },
-  {
-    id: 2,
-    title: "Code Chronicles",
-    author: "John Smith",
-    genre: "Technology",
-    description:
-      "An in-depth exploration of the history of coding, from the early pioneers to the modern day developers shaping our digital future.",
-    status: "unread",
-    image: "/mnt/data/A_single_technology_book_cover_titled_'Code_Chroni.png",
-  },
-  {
-    id: 3,
-    title: "Mystery of the Old Mansion",
-    author: "Clara Johnson",
-    genre: "Mystery",
-    description:
-      "A thrilling mystery that unfolds in a haunted mansion, where every room holds a new clue to a decades-old crime.",
-    status: "read",
-    image: "placeholder_for_mystery_mansion.png",
-  },
-  {
-    id: 4,
-    title: "Journey to the Stars",
-    author: "David Lee",
-    genre: "Science Fiction",
-    description:
-      "An epic space adventure following a crew of astronauts as they navigate the perils of deep space and discover new worlds.",
-    status: "unread",
-    image: "placeholder_for_journey_stars.png",
-  },
-  {
-    id: 5,
-    title: "Mindful Living",
-    author: "Sophie White",
-    genre: "Self-Help",
-    description:
-      "A guide to living a balanced life through mindfulness, meditation, and emotional well-being.",
-    status: "read",
-    image: "placeholder_for_mindful_living.png",
-  },
-  {
-    id: 6,
-    title: "Ocean's Whisper",
-    author: "Emily Stone",
-    genre: "Romance",
-    description:
-      "A heartwarming love story set against the backdrop of a small coastal town.",
-    status: "unread",
-    image: "placeholder_for_oceans_whisper.png",
-  },
-  {
-    id: 7,
-    title: "The Ancient Code",
-    author: "Michael Black",
-    genre: "Thriller",
-    description:
-      "An archaeological adventure to uncover a hidden ancient code that could change history.",
-    status: "read",
-    image: "placeholder_for_ancient_code.png",
-  },
-  {
-    id: 8,
-    title: "Life Hacks 101",
-    author: "Sarah Clark",
-    genre: "Self-Help",
-    description:
-      "A practical guide to simplifying daily life with smart tips and tricks.",
-    status: "unread",
-    image: "placeholder_for_life_hacks.png",
-  },
-  {
-    id: 9,
-    title: "Into the Abyss",
-    author: "Nathan Grey",
-    genre: "Horror",
-    description:
-      "A chilling horror story about a group of friends who descend into a haunted cave.",
-    status: "read",
-    image: "placeholder_for_into_the_abyss.png",
-  },
-  {
-    id: 10,
-    title: "The Future of AI",
-    author: "Laura White",
-    genre: "Technology",
-    description:
-      "A comprehensive exploration of the future possibilities of artificial intelligence.",
-    status: "unread",
-    image: "placeholder_for_future_of_ai.png",
-  },
-  {
-    id: 11,
-    title: "The Last Frontier",
-    author: "Mark Wilson",
-    genre: "Science Fiction",
-    description:
-      "A gripping tale of humanity's attempt to colonize the last habitable planet in a distant galaxy.",
-    status: "read",
-    image: "placeholder_for_last_frontier.png",
-  },
-  {
-    id: 12,
-    title: "Secrets of the Mind",
-    author: "Helen Reed",
-    genre: "Psychology",
-    description:
-      "An insightful look into the workings of the human brain and consciousness.",
-    status: "unread",
-    image: "placeholder_for_secrets_of_the_mind.png",
-  },
-  {
-    id: 13,
-    title: "Cooking with Love",
-    author: "Martha Dean",
-    genre: "Cooking",
-    description:
-      "Delicious recipes made with love for every occasion, from simple dinners to fancy desserts.",
-    status: "read",
-    image: "placeholder_for_cooking_with_love.png",
-  },
-  {
-    id: 14,
-    title: "Warriors of the East",
-    author: "Sam Carter",
-    genre: "Historical Fiction",
-    description:
-      "A tale of bravery and honor in ancient China, following the lives of legendary warriors.",
-    status: "unread",
-    image: "placeholder_for_warriors_of_the_east.png",
-  },
-  {
-    id: 15,
-    title: "Green Earth",
-    author: "Jessica Moore",
-    genre: "Environmental",
-    description:
-      "A guide to sustainable living and protecting our planet through everyday actions.",
-    status: "read",
-    image: "placeholder_for_green_earth.png",
-  },
-  {
-    id: 16,
-    title: "Shadows of the Night",
-    author: "Jake West",
-    genre: "Horror",
-    description:
-      "A bone-chilling horror novel where shadows come alive and prey on the fears of the townspeople.",
-    status: "unread",
-    image: "placeholder_for_shadows_of_night.png",
-  },
-  {
-    id: 17,
-    title: "Infinite Horizons",
-    author: "Olivia Brooks",
-    genre: "Adventure",
-    description:
-      "A thrilling adventure following a group of explorers as they embark on a journey across uncharted lands.",
-    status: "read",
-    image: "placeholder_for_infinite_horizons.png",
-  },
-  {
-    id: 18,
-    title: "The Quantum Leap",
-    author: "George Edwards",
-    genre: "Science Fiction",
-    description:
-      "A sci-fi novel about a physicist who makes a breakthrough in quantum mechanics, leading to unexpected consequences.",
-    status: "unread",
-    image: "placeholder_for_quantum_leap.png",
-  },
-  {
-    id: 19,
-    title: "Ancient Civilizations",
-    author: "Brian King",
-    genre: "History",
-    description:
-      "A comprehensive study of ancient civilizations and their influence on modern society.",
-    status: "read",
-    image: "placeholder_for_ancient_civilizations.png",
-  },
-  {
-    id: 20,
-    title: "The Art of Happiness",
-    author: "Emma Taylor",
-    genre: "Self-Help",
-    description:
-      "A self-help book offering advice on achieving lasting happiness through mindfulness and positive thinking.",
-    status: "unread",
-    image: "placeholder_for_art_of_happiness.png",
-  },
+[
   {
     id: 21,
     title: "Whispers of the Wind",
@@ -208,6 +8,7 @@ export const Books = [
       "A mystical journey of a young warrior guided by the whispers of the wind.",
     status: "unread",
     image: "placeholder_for_whispers_of_wind.png",
+    price: 15.99,
   },
   {
     id: 22,
@@ -218,6 +19,7 @@ export const Books = [
       "An exciting look into the world of cryptography and those who break the codes.",
     status: "read",
     image: "placeholder_for_code_breaker.png",
+    price: 12.49,
   },
   {
     id: 23,
@@ -228,6 +30,7 @@ export const Books = [
       "A chilling story of a family haunted by unseen forces in their new home.",
     status: "unread",
     image: "placeholder_for_haunted_nights.png",
+    price: 14.99,
   },
   {
     id: 24,
@@ -238,6 +41,7 @@ export const Books = [
       "A journey into the multiverse through the eyes of a quantum physicist.",
     status: "read",
     image: "placeholder_for_quantum_dreams.png",
+    price: 16.99,
   },
   {
     id: 25,
@@ -248,6 +52,7 @@ export const Books = [
       "A thrilling adventure to uncover a legendary artifact that has been lost for centuries.",
     status: "unread",
     image: "placeholder_for_lost_artifact.png",
+    price: 13.59,
   },
   {
     id: 26,
@@ -258,6 +63,7 @@ export const Books = [
       "A moving romance set against the vast, unpredictable waves of the sea.",
     status: "read",
     image: "placeholder_for_heart_of_ocean.png",
+    price: 9.99,
   },
   {
     id: 27,
@@ -268,6 +74,7 @@ export const Books = [
       "A thrilling tale of a man who discovers a diary that lets him travel through time.",
     status: "unread",
     image: "placeholder_for_time_travelers_diary.png",
+    price: 19.49,
   },
   {
     id: 28,
@@ -278,6 +85,7 @@ export const Books = [
       "A group of friends unearth hidden secrets on a remote island filled with danger.",
     status: "read",
     image: "placeholder_for_mysterious_islands.png",
+    price: 11.99,
   },
   {
     id: 29,
@@ -288,6 +96,7 @@ export const Books = [
       "An epic space opera following the journey of a starship exploring the outer reaches of the galaxy.",
     status: "unread",
     image: "placeholder_for_galactic_odyssey.png",
+    price: 18.79,
   },
   {
     id: 30,
@@ -298,6 +107,7 @@ export const Books = [
       "A historical novel intertwining the lives of two families across generations.",
     status: "read",
     image: "placeholder_for_echoes_of_past.png",
+    price: 14.49,
   },
   {
     id: 31,
@@ -308,6 +118,7 @@ export const Books = [
       "A riveting tale of cyber attacks and digital warfare in a near-future world.",
     status: "unread",
     image: "placeholder_for_cyber_wars.png",
+    price: 17.29,
   },
   {
     id: 32,
@@ -318,6 +129,7 @@ export const Books = [
       "An enchanted forest holds a dark secret waiting to be uncovered by a brave adventurer.",
     status: "read",
     image: "placeholder_for_dark_forest.png",
+    price: 10.99,
   },
   {
     id: 33,
@@ -328,6 +140,7 @@ export const Books = [
       "A suspenseful mystery about a quiet coastal town where dark secrets lie beneath the surface.",
     status: "unread",
     image: "placeholder_for_silent_waters.png",
+    price: 9.99,
   },
   {
     id: 34,
@@ -338,6 +151,7 @@ export const Books = [
       "A post-apocalyptic world where machines have taken over and a group of survivors fights back.",
     status: "read",
     image: "placeholder_for_rise_of_machines.png",
+    price: 13.79,
   },
   {
     id: 35,
@@ -348,6 +162,7 @@ export const Books = [
       "A young hero must break the curse that plagues a forgotten land.",
     status: "unread",
     image: "placeholder_for_cursed_lands.png",
+    price: 15.29,
   },
   {
     id: 36,
@@ -358,6 +173,7 @@ export const Books = [
       "A story of interdimensional travel and the chaos that unfolds when the realms collide.",
     status: "read",
     image: "placeholder_for_shattered_realms.png",
+    price: 11.49,
   },
   {
     id: 37,
@@ -368,6 +184,7 @@ export const Books = [
       "A terrifying tale of werewolves and the blood moon that drives them to madness.",
     status: "unread",
     image: "placeholder_for_crimson_moon.png",
+    price: 14.79,
   },
   {
     id: 38,
@@ -378,6 +195,7 @@ export const Books = [
       "An ancient secret is hidden within the blueprints of an old building.",
     status: "read",
     image: "placeholder_for_architects_secret.png",
+    price: 12.59,
   },
   {
     id: 39,
@@ -388,6 +206,7 @@ export const Books = [
       "A near-future world where virtual reality has taken over, leading to unexpected consequences.",
     status: "unread",
     image: "placeholder_for_virtual_revolution.png",
+    price: 16.99,
   },
   {
     id: 40,
@@ -398,205 +217,6 @@ export const Books = [
       "A thrilling expedition to the Antarctic uncovers a deadly secret hidden beneath the ice.",
     status: "read",
     image: "placeholder_for_beneath_the_ice.png",
-  },
-  {
-    id: 41,
-    title: "Chasing the Horizon",
-    author: "Madison Allen",
-    genre: "Adventure",
-    description:
-      "A group of explorers sets sail to find the edge of the world.",
-    status: "unread",
-    image: "placeholder_for_chasing_horizon.png",
-  },
-  {
-    id: 42,
-    title: "The Last Heir",
-    author: "Benjamin Scott",
-    genre: "Fantasy",
-    description:
-      "A young prince embarks on a quest to reclaim his kingdom from a dark sorcerer.",
-    status: "read",
-    image: "placeholder_for_last_heir.png",
-  },
-  {
-    id: 43,
-    title: "Uncharted Waters",
-    author: "Zoe Hunter",
-    genre: "Adventure",
-    description:
-      "A thrilling adventure of a crew that discovers uncharted lands on a treacherous voyage.",
-    status: "unread",
-    image: "placeholder_for_uncharted_waters.png",
-  },
-  {
-    id: 44,
-    title: "The Silent War",
-    author: "Harper Moore",
-    genre: "Historical Fiction",
-    description:
-      "A gripping tale of espionage and betrayal during World War II.",
-    status: "read",
-    image: "placeholder_for_silent_war.png",
-  },
-  {
-    id: 45,
-    title: "Frozen Shadows",
-    author: "Isabella Carter",
-    genre: "Thriller",
-    description:
-      "A chilling story of survival in the frozen wilderness, where shadows hide dangerous secrets.",
-    status: "unread",
-    image: "placeholder_for_frozen_shadows.png",
-  },
-  {
-    id: 46,
-    title: "Cosmic Drift",
-    author: "Lucas Reed",
-    genre: "Science Fiction",
-    description:
-      "A crew of astronauts discovers an alien presence drifting through the cosmos.",
-    status: "read",
-    image: "placeholder_for_cosmic_drift.png",
-  },
-  {
-    id: 47,
-    title: "Secrets of the Sands",
-    author: "Amelia Gray",
-    genre: "Adventure",
-    description:
-      "An archaeologist uncovers a hidden treasure buried deep in the sands of Egypt.",
-    status: "unread",
-    image: "placeholder_for_secrets_of_sands.png",
-  },
-  {
-    id: 48,
-    title: "The Phantom Ship",
-    author: "Henry Walker",
-    genre: "Horror",
-    description:
-      "A ghostly ship appears off the coast, and the crew is never seen again.",
-    status: "read",
-    image: "placeholder_for_phantom_ship.png",
-  },
-  {
-    id: 49,
-    title: "Rise of the Titans",
-    author: "Victoria Bell",
-    genre: "Fantasy",
-    description:
-      "In a world of magic and monsters, ancient titans rise from the earth to reclaim their lost power.",
-    status: "unread",
-    image: "placeholder_for_rise_of_titans.png",
-  },
-  {
-    id: 50,
-    title: "Broken Crown",
-    author: "Christopher Jones",
-    genre: "Historical Fiction",
-    description:
-      "A gripping story of the fall of an empire and the family torn apart by its collapse.",
-    status: "read",
-    image: "placeholder_for_broken_crown.png",
-  },
-  {
-    id: 51,
-    title: "Shadows in the Mist",
-    author: "Natalie Adams",
-    genre: "Mystery",
-    description:
-      "A small town detective uncovers dark secrets hidden in the misty woods.",
-    status: "unread",
-    image: "placeholder_for_shadows_in_mist.png",
-  },
-  {
-    id: 52,
-    title: "Digital Fortress",
-    author: "Ella Bennett",
-    genre: "Technology",
-    description:
-      "A hacker must take down a digital fortress to prevent a global catastrophe.",
-    status: "read",
-    image: "placeholder_for_digital_fortress.png",
-  },
-  {
-    id: 53,
-    title: "The Eternal Flame",
-    author: "Noah Parker",
-    genre: "Fantasy",
-    description:
-      "A chosen hero must protect the eternal flame, the source of all magic in the world.",
-    status: "unread",
-    image: "placeholder_for_eternal_flame.png",
-  },
-  {
-    id: 54,
-    title: "Into the Abyss",
-    author: "Liam Morgan",
-    genre: "Horror",
-    description:
-      "A deep-sea exploration turns into a nightmare as creatures from the abyss emerge.",
-    status: "read",
-    image: "placeholder_for_into_the_abyss.png",
-  },
-  {
-    id: 55,
-    title: "Fragments of Time",
-    author: "Chloe Evans",
-    genre: "Science Fiction",
-    description:
-      "A time traveler must collect fragments of time to prevent the collapse of reality.",
-    status: "unread",
-    image: "placeholder_for_fragments_of_time.png",
-  },
-  {
-    id: 56,
-    title: "The Forgotten Kingdom",
-    author: "Dylan Roberts",
-    genre: "Fantasy",
-    description:
-      "A hidden kingdom lost to time is rediscovered by a group of adventurers.",
-    status: "read",
-    image: "placeholder_for_forgotten_kingdom.png",
-  },
-  {
-    id: 57,
-    title: "Beyond the Stars",
-    author: "Grace Harris",
-    genre: "Science Fiction",
-    description:
-      "A lone astronaut journeys beyond the stars in search of a new home for humanity.",
-    status: "unread",
-    image: "placeholder_for_beyond_stars.png",
-  },
-  {
-    id: 58,
-    title: "The Secret Garden",
-    author: "Emily Davis",
-    genre: "Children's",
-    description:
-      "A magical garden is discovered by a group of children, leading to incredible adventures.",
-    status: "read",
-    image: "placeholder_for_secret_garden.png",
-  },
-  {
-    id: 59,
-    title: "The Rogue Prince",
-    author: "Ethan Brooks",
-    genre: "Historical Fiction",
-    description:
-      "A rebellious prince leads a revolution against a tyrannical king.",
-    status: "unread",
-    image: "placeholder_for_rogue_prince.png",
-  },
-  {
-    id: 60,
-    title: "Winds of Change",
-    author: "Mia Ross",
-    genre: "Romance",
-    description:
-      "A heartfelt romance set during a time of great social change.",
-    status: "read",
-    image: "placeholder_for_winds_of_change.png",
+    price: 14.49,
   },
 ];
