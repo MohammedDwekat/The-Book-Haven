6e3d9e0a18700a3fea3bfdeae42f999bec4f63c5
MohammedDwekat <mohammed.dwekat94@gmail.com>
2024-10-23 22:48:50 +0300

    use fetch to get data

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-import { Books } from "./data.js";
-
-const ViewBooks = (array = []) => {
-  console.log("Books list :\n");
-  array.forEach((Book) =>
-    console.log(` Book ${Book.id} ${Book.title} ,  ${Book.author}`)
-  );
-};
-
-ViewBooks(Books);
+fetch("data.json")
+  .then((response) => {
+    if (response.ok) return response.json();
+  })
+  .then((data) => {
+    const booklist = document.getElementById("book-list");
+    if (booklist) {
+      data.forEach((book) => {
+        const bookHTML = `<div class="book-card">
+              <h2>${book.title}</h2>
+              <h3>${book.author}</h3>
+              <p>${book.price}</p>
+              
+            </div>
+          `;
+        booklist.innerHTML += bookHTML;
+      });
+    }
+  })
+  .catch((error) => {
+    console.error("Problem with the fetch data:", error);
+  });
